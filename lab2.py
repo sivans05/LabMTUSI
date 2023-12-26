944b4ad747683b0dd751db13b537dc2815a5208b
sivans05 <147487186+sivans05@users.noreply.github.com>
2023-12-26 05:36:58 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI, Request, status
+
+app = FastAPI()
+
+
+# PATH запрос
+
+@app.get("/from_path/{equation}")
+def search_by_path(request: Request, equation: str):
+    for equation in equation.split():
+        if equation != "":
+            try:
+                equation = equation.replace(':', '/')
+                data = eval(equation)
+            except:
+                data = "ошибка в математическом выражении!"
+            return data, status.HTTP_200_OK
+
+    return status.HTTP_500_INTERNAL_SERVER_ERROR
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
